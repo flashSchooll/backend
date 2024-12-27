38fff348fe232d25ed10344e8234cf09f360f4b3
Osmnalyyyy <sevinc32isparta@gmail.com>
2024-12-27 17:39:11 +0300

    update findRandomCardsByBranch

--- src/main/java/com/flashcard/repository/CardRepository.java
+++ src/main/java/com/flashcard/repository/CardRepository.java
@@ -30,7 +30,7 @@ public interface CardRepository extends JpaRepository<Card, Long> {
             "JOIN Flashcard f ON c.flashcard_id = f.id " +
             "JOIN Topic t ON f.topic_id = t.id " +
             "JOIN Lesson l ON t.lesson_id = l.id " +
-            "WHERE (:branch is null or l.branch = :branch) " +
+            "WHERE (l.branch is null or (:branch is null or l.branch = :branch)) " +
             "ORDER BY RANDOM() LIMIT 100", nativeQuery = true)
     List<Card> findRandomCardsByBranch(@Param("branch") String branch);
 }
