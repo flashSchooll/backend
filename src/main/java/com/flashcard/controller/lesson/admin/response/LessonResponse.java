75d8adc2cf5d39f9bda1c253dda796a12afad7d2
Osmnalyyyy <sevinc32isparta@gmail.com>
2025-02-06 19:46:44 +0300

    update LessonResponse

--- src/main/java/com/flashcard/controller/lesson/admin/response/LessonResponse.java
+++ src/main/java/com/flashcard/controller/lesson/admin/response/LessonResponse.java
@@ -9,14 +9,14 @@ import lombok.Setter;
 public class LessonResponse {
 
     private final Long lessonId;
-    private final String name;
+    private final String yksLesson;
     private final String branch;
     private final String yks;
     private final byte[] icon;
 
     public LessonResponse(Lesson lesson) {
         this.lessonId = lesson.getId();
-        this.name = lesson.getYksLesson() != null ? lesson.getYksLesson().label : null;
+        this.yksLesson = lesson.getYksLesson() != null ? lesson.getYksLesson().label : null;
         this.icon = lesson.getIcon();
         this.branch = lesson.getBranch() != null ? lesson.getBranch().label : null;
         this.yks = lesson.getYks().name();
