1b28f07208bad9e57d91eaebad75e910cf2e33cc
Osmnalyyyy <sevinc32isparta@gmail.com>
2025-06-22 22:48:44 +0300

    added rate limit

--- src/main/java/com/flashcard/config/WebConfig.java
+++ src/main/java/com/flashcard/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.flashcard.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -17,4 +19,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
     }
 
+    @Autowired
+    private RateLimitInterceptor rateLimitInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(rateLimitInterceptor);
+    }
 }
