01a704ecb1db750e5aab4cd826e89194c7a33e3d
Osmnalyyyy <sevinc32isparta@gmail.com>
2025-11-09 17:48:14 +0300

    added yksLesson field to LessonListResponse

--- src/main/java/com/flashcard/controller/lesson/user/response/LessonListResponse.java
+++ src/main/java/com/flashcard/controller/lesson/user/response/LessonListResponse.java
@@ -1,6 +1,7 @@
 package com.flashcard.controller.lesson.user.response;
 
 import com.flashcard.model.Lesson;
+import com.flashcard.model.enums.YKSLesson;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,11 +12,13 @@ public class LessonListResponse {
     private final String lesson;
     private final String icon;
     private final String yks;
+    private final YKSLesson yksLesson;
 
     public LessonListResponse(Lesson lesson) {
         this.id = lesson.getId();
         this.lesson = lesson.getYksLesson().label;
         this.icon = lesson.getPath();
         this.yks = lesson.getYks().name();
+        this.yksLesson = lesson.getYksLesson();
     }
 }
