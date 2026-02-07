dcc984f7ed54e091cfc80e65e39c77a4d7d9649f
Osmnalyyyy <sevinc32isparta@gmail.com>
2026-02-07 19:47:03 +0300

    updated WebSecurityConfig

--- src/main/java/com/flashcard/security/WebSecurityConfig.java
+++ src/main/java/com/flashcard/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import com.flashcard.security.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -82,6 +83,7 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
 
                 /* 4) Yetkilendirme */
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(
                                 "/api/auth/**",
                                 "/api/test/**",
